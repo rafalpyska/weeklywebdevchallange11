7b7e8b70dea45ee438aecd93229ef64087f5816a
Rafał <rafix94@gmail.com>
2019-04-03 13:18:07 +0200

    Add 'offer' section css styles 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,3 @@
-let projectsProgress = new ProgressBar.Circle(projects, {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 2000,
-    color: 'red',
-    trailColor: '#EEE',
-    trailWidth: 1,
-    svgStyle: null
-});
-
-let clientsProgress = new ProgressBar.Circle(clients, {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 2000,
-    color: 'green',
-    trailColor: '#EEE',
-    trailWidth: 1,
-    svgStyle: null
-});
-
-let monthsProgress = new ProgressBar.Circle(months, {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 2000,
-    color: 'black',
-    trailColor: '#EEE',
-    trailWidth: 1,
-    svgStyle: null
-});
-
 (function() {
     let nav = document.querySelector(".navigation__expanded");
     let navigation__toggle = document.querySelector(".navigation__toggle");
@@ -68,11 +38,6 @@ function smoothScroll(target, duration) {
     }
     requestAnimationFrame(scrollAnimation);
 }
-
-
-projectsProgress.animate(0.5);
-clientsProgress.animate(0.35);
-monthsProgress.animate(0.80);
 
 let scrollDown = document.querySelector('.scroll-down');
 scrollDown.addEventListener('click', function() {
