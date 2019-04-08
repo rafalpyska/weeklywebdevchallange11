d857868622080547de6ee37174ddf77b166bd2b8
Rafał <rafix94@gmail.com>
2019-04-08 19:41:44 +0200

    Add two classes - 'CreateDestination' and 'DestinationList' with some static functions to create more easily new places to visit

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,73 @@
     datePicker.valueAsDate = new Date();
 
     const items = document.querySelectorAll('.trip-planner__destination--filled');
-    const itemsLength = items.length;
     const empty = document.querySelectorAll('.trip-planner__destination--empty');
+    const destinationList = document.querySelector('.trip-planner__destination-list');
 
-    for (let i = 0; i < itemsLength; i++) {
-        items[i].setAttribute('draggable', 'true');
+    class CreateDestination {
+        constructor(place, time, image) {
+            this.place = place;
+            this.time = time;
+            this.image = image;
+        }
     }
+
+    class DestinationList {
+        static displayDestination() {
+            const StoredDestination = [
+                {
+                    place: 'Mt. Bromo',
+                    time: 'estimated time: 5 hours',
+                    image: 'images/svg/mountains.svg'
+                },
+                {
+                    place: 'Mt. Semeru',
+                    time: 'estimated time: 3 hours',
+                    image: 'images/svg/mountains.svg'
+                },
+                {
+                    place: 'Mt. Panderman',
+                    time: '4 hours',
+                    image: 'images/svg/mountains.svg'
+                }
+            ];
+            const places = StoredDestination;
+
+            places.forEach((destination) => DestinationList.addDestination(destination));
+        }
+        static addDestination(destination) {
+            const docFrag = document.createDocumentFragment();
+            const divOuter = docFrag.appendChild(document.createElement('div'));
+            const divInner = divOuter.appendChild(document.createElement('div'));
+
+            divOuter.classList.add('trip-planner__destination', 'trip-planner__destination--empty','trip-planner__destination--list-empty');
+            divInner.classList.add('trip-planner__destination', 'trip-planner__destination--filled');
+
+            divInner.setAttribute('draggable', 'true');
+
+            divInner.innerHTML = `
+                            <div class="">
+                                <p>${destination.place}</p>
+                                <p>estimated time: <span class="heading-color">${destination.time}</span></p>
+                            </div>
+                            <div class="">
+                                <object type="image/svg+xml" data="${destination.image}"></object>
+                                <a href="#">view details</a>
+                            </div>
+                        </div>
+            `
+            destinationList.appendChild(docFrag);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', DestinationList.displayDestination);
+
 
     let item = null;
 
     document.addEventListener('dragstart', function(e) {
         item = e.target;
         item.style.opacity = '0.4';
-        item.classList.add('hold');
         e.dataTransfer.effectAllowed = 'move';
         e.dataTransfer.setData('text', '');
 
