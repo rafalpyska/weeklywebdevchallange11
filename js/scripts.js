4f596e535a82e56280240a498f0c7745964819a8
Rafał <rafix94@gmail.com>
2019-04-10 21:43:05 +0200

    Changes in trip planner

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,6 @@
     const datePicker = document.querySelector('.trip-planner__datepicker');
     datePicker.valueAsDate = new Date();
 
-    const items = document.querySelectorAll('.trip-planner__destination--filled');
-    const empty = document.querySelectorAll('.trip-planner__destination--empty');
-    const destinationList = document.querySelector('.trip-planner__destination-list');
-    const destinationContainer = document.querySelector('.trip-planner__destination-container');
 
     class CreateSubDestination {
         constructor(subdestination) {
@@ -85,56 +81,94 @@
         window.print();
     }, false);
 
-    // class DestinationList {
-    //     static displayDestination() {
-    //         const StoredDestination = [
-    //             {
-    //                 place: 'Mt. Bromo',
-    //                 time: 'estimated time: 5 hours',
-    //                 image: 'images/svg/mountains.svg'
-    //             },
-    //             {
-    //                 place: 'Mt. Semeru',
-    //                 time: 'estimated time: 3 hours',
-    //                 image: 'images/svg/mountains.svg'
-    //             },
-    //             {
-    //                 place: 'Mt. Panderman',
-    //                 time: '4 hours',
-    //                 image: 'images/svg/mountains.svg'
-    //             }
-    //         ];
-    //         const places = StoredDestination;
-    //
-    //         places.forEach((destination) => DestinationList.addDestination(destination));
-    //     }
-    //     static addDestination(destination) {
-    //         const docFrag = document.createDocumentFragment();
-    //         const divOuter = docFrag.appendChild(document.createElement('div'));
-    //         const divInner = divOuter.appendChild(document.createElement('div'));
-    //
-    //         divOuter.classList.add('trip-planner__destination', 'trip-planner__destination--empty','trip-planner__destination--list-empty');
-    //         divInner.classList.add('trip-planner__destination', 'trip-planner__destination--filled');
-    //
-    //         divInner.setAttribute('draggable', 'true');
-    //
-    //         divInner.innerHTML = `
-    //                         <div class="trip-planner__destination-info">
-    //                             <p class="trip-planner__destination-info-paragraph">${destination.place}</p>
-    //                             <p class="trip-planner__destination-info-paragraph">estimated time: <span class="heading-color">${destination.time}</span></p>
-    //                         </div>
-    //                         <div class="trip-planner__destination-details">
-    //                             <object type="image/svg+xml" data="${destination.image}"></object>
-    //                             <a href="#" class="trip-planner__destination-details-link">view details</a>
-    //                         </div>
-    //                     </div>
-    //         `;
-    //         destinationList.appendChild(docFrag);
-    //     }
-    // }
-    //
-    // document.addEventListener('DOMContentLoaded', DestinationList.displayDestination);
 
+    class DestinationList {
+        static displayDestination() {
+            const StoredDestination = [
+                {
+                    place: 'Mt. Bromo',
+                    time: '5 hours',
+                    image: 'images/svg/mountains.svg',
+                    id: 1
+                },
+                {
+                    place: 'Mt. Semeru',
+                    time: '3 hours',
+                    image: 'images/svg/mountains.svg',
+                    id: 2
+                },
+                {
+                    place: 'Mt. Panderman',
+                    time: '4 hours',
+                    image: 'images/svg/mountains.svg',
+                    id: 3
+                }
+            ];
+            const places = StoredDestination;
+
+            places.forEach((destination) => DestinationList.addDestination(destination));
+        }
+        static addDestination(destination) {
+            const docFrag = document.createDocumentFragment();
+            const divOuter = docFrag.appendChild(document.createElement('div'));
+            const divInner = divOuter.appendChild(document.createElement('div'));
+
+            divOuter.classList.add('trip-planner__destination', 'trip-planner__destination--empty','trip-planner__destination-list--empty');
+            divInner.classList.add('trip-planner__destination', 'trip-planner__destination--filled', 'trip-planner__destination-list--filled');
+            divInner.setAttribute("data-value",`${destination.id}"`);
+
+            divInner.setAttribute('draggable', 'true');
+
+            divInner.innerHTML = `
+                            <div class="trip-planner__destination-info">
+                                <h2 class="trip-planner__destination-info-heading">${destination.place}</h2>
+                                <p class="trip-planner__destination-info-paragraph">estimated time: <span class="heading-color">${destination.time}</span></p>
+                            </div>
+                            <div class="trip-planner__destination-details">
+                                <object type="image/svg+xml" data="${destination.image}"></object>
+                                <a href="#" class="trip-planner__destination-details-link">view details</a>
+                            </div>
+                        </div>
+            `;
+            destinationList.appendChild(docFrag);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', DestinationList.displayDestination);
+
+    const filledContainers = document.querySelectorAll('.trip-planner__destination--filled');
+    // console.log(filledContainers);
+    const emptyContainers = document.querySelectorAll('.trip-planner__destination--empty');
+    // console.log(emptyContainers);
+    const destinationList = document.querySelector('.trip-planner__destination-list');
+    const destinationContainer = document.querySelector('.trip-planner__destination-container');
+
+    // let item = null;
+    //
+    // for(const filled of filledContainers) {
+    //     filled.addEventListener('dragstart', dragStart);
+    //     filled.addEventListener('dragEnd', dragEnd);
+    // }
+    // for (const empty of emptyContainers) {
+    //     empty.addEventListener('dragover', dragOver);
+    //     // empty.addEventListener('dragenter', dragEnter);
+    //     // empty.addEventListener('dragleave', dragLeave);
+    //     empty.addEventListener('drop', dragDrop);
+    // }
+    // function dragStart(e) {
+    //     item = e.target;
+    //     const value = e.target.getAttribute('data-value');
+    //     e.dataTransfer.setData('text', value);
+    //     e.dataTransfer.effectAllowed = 'copy';
+    // }
+    // function dragOver(e) {
+    //     e.preventDefault();
+    // }
+    // function dragDrop(e) {
+    //     e.preventDefault();
+    //     e.target.appendChild(item);
+    //     e.dataTransfer.getData('text');
+    // }
     //
     // let item = null;
     //
