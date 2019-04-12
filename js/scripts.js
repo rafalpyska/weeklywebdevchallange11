d38a0404b507c6cfc2e9d582c420fdce9213465c
Rafał <rafix94@gmail.com>
2019-04-12 23:11:55 +0200

    Add possibility to switch bewteen 'all place' and 'recommended place' tabs

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,24 @@ nav_toggle.addEventListener("click", function () {
     }
 }, false);
 
+const tabs = document.querySelector('.offer__list');
+const offers = document.querySelectorAll('.offer-city-list');
+const offerRecommended = document.getElementById('offer__recommended');
+const offerPagination = document.querySelector('.offer-pagination');
+
+tabs.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI') {
+        const targetOffer = document.querySelector(e.target.dataset.target);
+        offers.forEach((offer) => {
+            if (offer === targetOffer) {
+                offer.classList.add('offer-city-list--active');
+            } else {
+                offer.classList.remove('offer-city-list--active');
+            }
+        })
+    }
+}, false);
+
 
 const datePicker = document.querySelector('.trip-planner__datepicker');
 datePicker.valueAsDate = new Date();
