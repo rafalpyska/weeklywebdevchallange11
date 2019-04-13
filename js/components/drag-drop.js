f54ecf48f66698d4091286398228a602d9ff5561
Rafał <rafix94@gmail.com>
2019-04-13 15:04:34 +0200

    Try to fix mobile drag and drop issues (window scrolling when dragging element)

--- js/components/drag-drop.js
+++ js/components/drag-drop.js
@@ -1,9 +1,22 @@
 export class DragDrop {
     static init() {
+        let scrollable = true;
+        const listener = function(e) {
+            if(!scrollable) {
+                e.preventDefault();
+            }
+        }
+        document.addEventListener('touchmove', listener, { passive: false });
         dragula([
             document.getElementById('draggable2'),
-            document.getElementById('draggable1'), {
-            }]);
+            document.getElementById('draggable1')
+            ]).on('drag', function(el,source) {
+                scrollable = false;
+            }).on('drop', function(el, source) {
+                scrollable = true;
+            }).on('dragend', function(el, source) {
+                scrollable = true;
+            });
     }
 }
 
